2effdefa9f226dbcdab3c404e8d0b95424cabdb3
jsooOO <35682236+kjsu0209@users.noreply.github.com>
2020-12-27 21:50:48 +0900

    Create template.py

---
+++ algospot/template.py
@@ -0,0 +1,14 @@
+# 테스트케이스 입력 템플릿
+
+test_case = int(input())
+
+for test_no in test_case:
+    n = int(input())
+    # ...
+    # put your solution code
+    # ...
+    print(n)
+    # ...
+    # print your answer of each testcase
+    # ...
+    continue
