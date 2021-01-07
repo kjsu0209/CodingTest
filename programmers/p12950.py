135144075e6e03a1d2aa8f52f38ffafef6d35c5e
jsooOO <35682236+kjsu0209@users.noreply.github.com>
2021-01-07 22:47:20 +0900

    Create p12950.py

---
+++ programmers/p12950.py
@@ -0,0 +1,7 @@
+# 0:01
+def solution(arr1, arr2):
+    answer = [[] for _ in range(len(arr1))]
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            answer[i].append(arr1[i][j] + arr2[i][j])
+    return answer
