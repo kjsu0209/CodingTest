3a3970a5868c62cb7b1f8b0133668fd6a14594b0
jsooOO <35682236+kjsu0209@users.noreply.github.com>
2021-01-16 20:58:02 +0900

    Create d1_2046.java

---
+++ swexpert/d1_2046.java
@@ -0,0 +1,19 @@
+// 0:01
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+        for(int i=0;i<num;i++){
+        	System.out.print("#");
+        }
+        System.out.println();
+	}
+}
