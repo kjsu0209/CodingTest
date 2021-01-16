d79e7abda3d4cd74080b03f93646ac060ab8dc7d
jsooOO <35682236+kjsu0209@users.noreply.github.com>
2021-01-16 21:21:22 +0900

    Create d1_1938.java

---
+++ swexpert/d1_1938.java
@@ -0,0 +1,20 @@
+// 0:01
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+	
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+	}
+}
