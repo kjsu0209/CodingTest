60bdbd6e94b9b94008ae99486aa04860f49dab6e
jsooOO <35682236+kjsu0209@users.noreply.github.com>
2021-01-21 23:14:13 +0900

    Create b11653.java

---
+++ baekjoon/b11653.java
@@ -0,0 +1,23 @@
+// 0:02
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		// 일반 반복문
+		int num = 2;
+		while(num <= N) {
+			if(N%num == 0) {
+				System.out.println(num);
+				N /= num;
+			}else {
+				num ++;
+			}
+		}
+	}
+
+}
