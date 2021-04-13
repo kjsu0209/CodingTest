7ded721384e1abfc6cc9df498d0c90d42dc76968
jsooOO <35682236+kjsu0209@users.noreply.github.com>
2021-04-13 22:56:49 +0900

    4월 13일

---
+++ baekjoon/b11399.java
@@ -0,0 +1,14 @@
+package CodingTest.baekjoon;
+import java.io.*;
+import java.util.*;
+public class b11399 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+	}
+
+}
